264fa8d4591b3a531cbe03b7c7f7cd3fa04fa4c0
pdichone <pdichone@gmail.com>
2021-10-06 21:46:18 -0700

    Delete Quotes Repository

--- app/src/main/java/com/bawp/quotes_test/repository/QuotesRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.bawp.quotes_test.repository
-
-import com.bawp.quotes_test.network.QuotesApi
-import javax.inject.Inject
-
-class QuotesRepository @Inject constructor(
-       private val api: QuoteApi
-                                          ) {}
